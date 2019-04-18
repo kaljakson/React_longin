a423afaaf5e5ea711ccfaf41bbe3841f3a9f754c
jaksonkal <jaksonkal1993@gmail.com>
2019-04-18 18:14:45 +0800

    install react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Login from './Components/login';
 import Home from './Components/homepage';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Button } from 'reactstrap';
@@ -11,14 +12,38 @@ class App extends Component {
   constructor(props){
     super();
     this.state = {
-      loginInfo: true
+      loginInfo: false
     }
   }
   render() {
     return (
+      // <div>
+      //   {this.state.loginInfo?<Home />:<Login />}
+      // </div>
+      // <Router>
+      //   <div>
+      //     <ul>
+      //       <li><Link to={'/'}>Home</Link></li>
+      //       <li><Link to={'/login'}>Login</Link></li>
+      //     </ul>
+      //     <hr/>
+      //       <Route exact path='/' Component={Home}/>
+      //       <Route exact path='/login' Component={Login}/>
+      //   </div>
+      // </Router>
+      <Router>
       <div>
-        {this.state.loginInfo?<Login />:<Home />}
+        <nav>
+          <ul>
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/login'}>Login</Link></li>            
+          </ul>
+        </nav>
+
+        <Route exact path='/' Component={Home}/>
+        <Route exact path='/login' Component={Login}/>
       </div>
+    </Router>
     );
   }
 }
